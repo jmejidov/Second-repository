55a613732299a9f6e6a4623edecf6e906840201a
Jem Mejidov <36999964+jmejidov@users.noreply.github.com>
2020-05-19 00:28:15 -0400

    another one

--- src/arrays/MaximumValueinARow.java
+++ src/arrays/MaximumValueinARow.java
@@ -3,18 +3,30 @@ import java.util.Arrays;
 
 public class MaximumValueinARow {
     public static void main(String[] args) {
+        int[][] newArr = {{1,4},{3,4}};
+        int [][] returnArr = switchRowsArr(newArr);
 
+        for (int [] row : returnArr ){
+
+            for (int num : row ){
+
+                System.out.print(num);
+            }
+            System.out.println();
+        }
+
+        /*
         int [][] myArray = {{1,2,3,9},{4,5,6,7},{7,8,9,7}};
 
         int [] [] newArr = {{1,2,3,9,12},{4,5,6,7,11},{10,7,8,9,7}};
 
         int arr [] = maxRowReturn(newArr);
-
-        System.out.println(Arrays.toString(arr));
+        String arrStr = Arrays.toString(arr);
+        System.out.println(arrStr);
 
         for (int num : arr ){
             System.out.print(num);
-        }
+        }*/
 
 
     }
@@ -31,7 +43,7 @@ public class MaximumValueinARow {
 
             for (int i = 0 ; i< arr.length ; i++ ){
 
-                if (arr[i]>maxVal ){
+                if (arr[i]<maxVal ){
 
                     maxVal = arr[i];
                 }
@@ -41,6 +53,23 @@ public class MaximumValueinARow {
 
         }
         return maxes;
+
+    }
+
+    static int [][] switchRowsArr (int [] [] arr ){
+
+        int [][]  arrReturn = new int [2][2];
+        int index = 0;
+        for (int [] row : arr ){
+
+            for (int i = 0 ; i < row.length ; i++ ){
+
+                arrReturn[index][i] = arr [i][index] ;
+
+            }
+            index++;
+        }
+        return arrReturn;
 
     }
 
